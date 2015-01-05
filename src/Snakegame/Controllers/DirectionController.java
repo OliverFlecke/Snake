4cee47e7a5efcf7cc065526ed7bdc776584267b5
Oliver Fleckenstein <Oliverfl@live.dk>
2015-01-05 13:30:16 +0100

    Updated DirectionController

--- src/Snakegame/Controllers/DirectionController.java
+++ src/Snakegame/Controllers/DirectionController.java
@@ -11,7 +11,23 @@ public class DirectionController implements KeyListener {
 	@Override
 	public void keyPressed(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+		switch (e.getID()) {
+			case KeyEvent.VK_DOWN:
+				
+				break;
+			case KeyEvent.VK_UP:
+				
+				break;
+			case KeyEvent.VK_LEFT:
+				
+				break;	
+			case KeyEvent.VK_RIGHT:
+				
+				break;
+				
+			default:
+					break;
+		}
 	}
 
 	@Override
