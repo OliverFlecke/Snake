1e95ad5de8354ee07558ac9a3b5e2ceef8195a57
sebakeaaen <sebakeaaen@gmail.com>
2015-01-06 16:05:37 +0100

    Revert "Revert "Changes to game controller""
    This reverts commit 0b09e667e7e5e7329aedd58f70184fd6d08082c0.

--- src/Snakegame/Models/Game.java
+++ src/Snakegame/Models/Game.java
@@ -1,35 +1,38 @@
 package snakegame.models;
 
 import java.awt.Dimension;
+import java.awt.Point;
+import java.util.List;
 import java.util.ArrayList;
+
+import snakegame.controllers.DirectionController;
+import snakegame.controllers.GameListener;
 
 /**
  *	Game model with all the game rules and logic
  */
 public class Game {
+	//List of viewers to notify of updates
+	List<GameListener> listeners = new ArrayList<GameListener>();
 	// The snake player
 	private Snake snake;
 	// ArrayList of all the food objects on the game field
 	private ArrayList<Food> food;
-	
+
 	private static Dimension size;
-	
+
 	/**
 	 * Constructor which takes the size of the game and stores it
 	 * @param size Size of the game board
 	 */
 	public Game(Dimension newSize) {
 		size = newSize;
-		this.food = new ArrayList<Food>();
-		food.add(new Food(1));
 		this.snake = new Snake();
-		mainGame();
+		//creates initial food item and list
+		this.food = new ArrayList<Food>();
+		createFoodInGame(1);
+		//creates listener for snake and game
 		new DirectionController(snake,this);
-	}
-	
-	private void mainGame() {
-		
-		
 	}
 
 	/**
@@ -40,7 +43,7 @@ public class Game {
 	public Game(int width, int height) {
 		this(new Dimension(width, height));
 	}
-	
+
 	/**
 	 * Get the dimension of the game 
 	 * @return The dimensions of the game
@@ -48,24 +51,62 @@ public class Game {
 	public static Dimension getDimension() {
 		return size;
 	}
-	
+
 	/**
 	 * Remove this food object passed to method
 	 * @param food Food object to delete 
 	 */
 	public void removeFood(Food eatenFood) {
 		this.food.remove(eatenFood);
-		eatenFood = null;
-		
-		// Create a new food object, so there always is a food object in the game
+
+		// Creates a new food object, so there is always a food object in the game
 		this.createFoodInGame(1);
 	}
-	
+
 	/**
 	 * Create a new food object
 	 * @param foodValue Value to give the new food object
 	 */
-	public void createFoodInGame(int foodValue) {
+	private void createFoodInGame(int foodValue) {
 		this.food.add(new Food(foodValue));
 	}
+	
+	/**
+	 * Updates the game following a snake movement, checking for collision with food and with the snake itself
+	 */
+	public void update() {
+		//ends game if snake collides with itself
+		if(this.snake.checkCollision()){
+			endGame();
+		}
+		/**
+		 * removes food if in collision with snake head, and generates new food.
+		 * Also notifies viewer of update
+		 */
+		for(Food current : this.food){
+			if(current.getPosition()==this.snake.getHead()){
+				removeFood(current);
+			}
+		}
+		notifyListener();
+
+	}
+	public void addListener(GameListener toAdd) {
+        listeners.add(toAdd);
+    }
+	private void notifyListener() {
+		for (GameListener gl : listeners)
+            gl.update();
+		
+	}
+	private void endGame() {
+		for (GameListener gl : listeners)
+            gl.endGame();
+		
+	}
+
+	public ArrayList<Point> getOccupiedCells() {	
+		Arraylist<Point> occupiedCells = snake.getPosition();
+	}
+	
 }
