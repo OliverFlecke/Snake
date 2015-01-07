b1631114e2f7b58e706460e0165a87b3c90bb5a1
Senbyo <markbj@gmail.com>
2015-01-07 14:54:50 +0100

    working on endGame Dialog

--- src/Snakegame/viewers/TestDriver.java
+++ src/Snakegame/viewers/TestDriver.java
@@ -1,8 +1,11 @@
 package snakegame.viewers;
 
+import snakegame.models.Game;
+
 public class TestDriver {
 
 	public static void main(String[] args) {
+		//GameDialog gameDia = new GameDialog(new Game(10, 10));
 		View gui = new View(10, 10);
 	}
 }
---
+++ src/snakegame/viewers/GameDialog.java
@@ -0,0 +1,58 @@
+package snakegame.viewers;
+
+import java.awt.GridLayout;
+import java.awt.Dimension;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import snakegame.models.Game;
+
+
+
+public class GameDialog extends JFrame{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1124153227936952372L;
+	
+	//Create the buttons
+	JButton restartBtn = new JButton("Restart");
+	JButton closeBtn = new JButton("Close");
+	
+	//Make a JPanel to hold the btns
+	JPanel btnShelf = new JPanel();
+	
+	//Make JLabel
+	JLabel scoreText = new JLabel();
+	
+	public GameDialog(Game game){
+		super();
+		int score = game.getScore();
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		btnShelf.add(restartBtn);
+		btnShelf.add(closeBtn);
+		add(btnShelf);
+		this.setIconImage(new ImageIcon("icon.png").getImage());
+		this.setPreferredSize(getPreferredSize());
+		this.pack();
+		this.setLocationRelativeTo(null);
+		this.setVisible(true);
+		
+		
+	}
+		
+		
+
+	
+	@Override
+	public Dimension getPreferredSize(){
+		return new Dimension(300, 150);
+
+		
+	}
+}
