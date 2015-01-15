01b681caab8025c52b92aee14af43e1e27047c4d
sebakeaaen <sebakeaaen@gmail.com>
2015-01-15 15:17:25 +0100

    Score på game dialog

--- src/snakegame/viewers/ScoreText.java
+++ src/snakegame/viewers/ScoreText.java
@@ -1,10 +1,11 @@
 package snakegame.viewers;
 
 import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.GridLayout;
 
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.SwingConstants;
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 
 import snakegame.models.Game;
 import snakegame.models.Snake;
@@ -16,10 +17,19 @@ public class ScoreText extends JPanel{
 	public ScoreText(Game game){
 		this.game = game;
 		this.setOpaque(false);
-		this.setLayout(new FlowLayout());
+		if(game.getSnakes().size()<=2){
+			this.setLayout(new GridLayout(2,1));
+		}
+		else{
+			this.setLayout(new GridLayout(2,2));
+		}
 		
-		for(Snake current : game.getSnakes()){
-			this.add(new JLabel("    " + current.getName() + " | " + current.getScore() + "    ", SwingConstants.CENTER));
+		
+		for(Snake current : game.getSnakes()) {
+			JLabel label = new JLabel(current.getName() + " | " + "Score:" + current.getScore(), SwingConstants.CENTER);
+			label.setFont(new Font(label.getFont().getFontName(), Font.BOLD, 13));
+			this.add(label);
+			//label.setBorder(new EmptyBorder(10, 20, 10, 20) );
 		}
 	}
 
