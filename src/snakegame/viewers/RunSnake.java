5abb435582c0b36690760e01fdeef1786ac6fc08
sebakeaaen <sebakeaaen@gmail.com>
2015-01-18 21:26:05 +0100

    Made it so only one dialog can be open at the time

--- src/snakegame/viewers/RunSnake.java
+++ src/snakegame/viewers/RunSnake.java
@@ -18,7 +18,11 @@ import javax.swing.SwingConstants;
  * Startup for the snake game, which includes settings for the game
  */
 public class RunSnake extends GenericDialog{
-	
+
+	ControlsDialog controlsDialog;
+	StandAloneHighScore standAloneHighScore;
+
+
 	/**
 	 * Start of the snake game. Main method for the entire application
 	 * @param args
@@ -27,35 +31,35 @@ public class RunSnake extends GenericDialog{
 		//new RunSnake();
 		new RunSnake();
 	}
-	
+
 	private static final long serialVersionUID = -647167119165272436L;
-	
+
 	//Set up the playerPanel for singleplayer
 	PlayerPanel playerPanel = new PlayerPanel(1);
-	
+
 	//List of names for player creation in game
 	ArrayList<String> names = new ArrayList<String>();
-		
+
 	//Create the buttons
 	JButton startGameBtn = new BigButton("Start Game");
 	JButton controlsBtn = new BigButton("Controls");
 	JButton highScoreBtn = new BigButton("HighScore");
-	
+
 	//creating a combobox
 	String[] numbOfPlayers = {"1","2","3","4"};
 	JComboBox<String> comboPlayers = new JComboBox<String>(numbOfPlayers);
-	
+
 	//Make JLabel
 	JLabel snake = new JLabel("", SwingConstants.CENTER);
 
 	//Make a JPanel to hold the btns and the labels
 	JPanel btnShelf = new JPanel();
 	JPanel dimensionsShelf = new JPanel();
-	
+
 	//Make textJpanels
 	TextJPanel widthPanel = new TextJPanel("Width:");
 	TextJPanel heightPanel = new TextJPanel("Height:");
-	
+
 	/**
 	 * Constructor. Creates the start dialog with settings for the snake game
 	 */
@@ -63,7 +67,7 @@ public class RunSnake extends GenericDialog{
 		super("Start Snake");
 		//Setting up combobox
 		comboPlayers.setSelectedIndex(0);
-		
+
 		//Set labelName for txtPanels.
 		widthPanel.setTxt(Integer.toString(30));
 		heightPanel.setTxt(Integer.toString(30));
@@ -72,30 +76,30 @@ public class RunSnake extends GenericDialog{
 		startGameBtn.setPreferredSize(new Dimension(100,30));
 		controlsBtn.setPreferredSize(new Dimension(100,30));
 		highScoreBtn.setPreferredSize(new Dimension(100,30));
-		
-		
+
+
 		//Making shelfs transparent
 		btnShelf.setOpaque(false);
 		dimensionsShelf.setOpaque(false);
-		
-		
+
+
 		//Setting up layout and adding buttons to shelf
 		btnShelf.setLayout(new FlowLayout());
 		btnShelf.add(startGameBtn);
 		btnShelf.add(highScoreBtn);
 		btnShelf.add(controlsBtn);
-		
+
 		snake.setPreferredSize(new Dimension(300,40));
 		dimensionsShelf.setPreferredSize(new Dimension(300,40));
-		
+
 		//Set up enterkey to trigger Jbutton. Used to start the game faster
 		startGameBtn.setMnemonic(KeyEvent.VK_ENTER);
-		
+
 		//setting up the dimensionsShelf
 		dimensionsShelf.setLayout(new FlowLayout());
 		dimensionsShelf.add(widthPanel);
 		dimensionsShelf.add(heightPanel);
-		
+
 		background.setLayout(new GridLayout(5,4));
 		background.add(snake);
 		background.add(new PlayerSelectPanel(playerPanel));
@@ -104,7 +108,7 @@ public class RunSnake extends GenericDialog{
 		background.add(btnShelf);
 
 		this.pack();
-		
+
 		/**
 		 * Listener for the start game button. Checks the input in the fields, and creates a view
 		 * with the given input
@@ -118,40 +122,48 @@ public class RunSnake extends GenericDialog{
 					width = 100;
 				else if (width < 5) 
 					width = 5;
-				
+
 				int height = Integer.parseInt(heightPanel.getTxt());
 				if (height > 100)
 					height = 100;
 				else if (height < 5) 
 					height = 5;
-				
+
 				// Insures the names don't have whitespace
 				ArrayList<String> playerNames = new ArrayList<String>();
 				for (String name : playerPanel.names()) {
 					playerNames.add(name.replaceAll("\\s+", ""));
 				}
-				
+
 				// Create the view with the game
 				new View(width, height,	playerNames);
 				closeFrame();
 			}
 		});
-		
+
 		/**
 		 * Listener for the highscore. Displays the highscore if pressed
 		 */
 		highScoreBtn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				new StandAloneHighScore();
+				if (controlsDialog == null) {
+					standAloneHighScore = new StandAloneHighScore();
+
+				}
+				standAloneHighScore.setVisible(true);
 			}
 		});
-		
+
 		/**
 		 * Listener for controls button. Shows the controls for the four players in the game
 		 */
 		controlsBtn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				new ControlsDialog();
+				if (controlsDialog == null) {
+					controlsDialog = new ControlsDialog();
+
+				}
+				controlsDialog.setVisible(true);
 			}
 		});		
 	}		
